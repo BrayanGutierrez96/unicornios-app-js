0228a0aed737e631f7c254c9309269f3a041ad22
BrayanGutierrez96 <brayangutierrezndev@gmail.com>
2024-12-12 17:28:40 -0500

    Se cambia la ruta del archivo main.js

--- public/main.js
+++ public/main.js
@@ -1,4 +1,4 @@
-import obtenerUnicornios from "../src/api/obtenerUnSoloUnicornio.js";
+import obtenerUnicornios from "../src/api/obtenerUnicornios.js";
 const contenedor = document.querySelector("#contenedor")
 const URL = "https://example-unicorns-api.vercel.app/unicornios";
 
